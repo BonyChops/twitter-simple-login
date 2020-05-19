7dbf8bcef0b52331b5567ebd6dc332b86a02fc4a
Bony_Chops <office.kentarou@gmail.com>
2020-05-20 01:09:16 +0900

    Edit: some name changed

--- define.php
+++ define.php
@@ -5,6 +5,6 @@ if(!file_exists(__DIR__."/config.json")){
     exit;
 }
 $data = json_decode(file_get_contents(__DIR__."/config.json"), true);
-define("Consumer_Key", $data['accessToken']);
-define("Consumer_Secret", $data['accessTokenSecret']);
-define('Callback', $data['redirectURL']);
+define("Consumer_Key", $data['ConsumerKey']);
+define("Consumer_Secret", $data['ConsumerSecretKey']);
+define('Callback', $data['CallbackURL']);
